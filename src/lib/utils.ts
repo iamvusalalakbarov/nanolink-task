9545535a7273c6e539c677f14e169c5bc2ef2b7e
Vusal <iamvusalalakbarov@gmail.com>
2025-04-25 01:48:35 +0400

    Created helper utility function to format names

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatName = (fullName: string) => {
+  const [firstName, lastName] = fullName.split(' ');
+  return `${firstName} ${lastName.charAt(0)}.`;
+};
