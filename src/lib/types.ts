34de4e03f4b838bef970bddd7e0718cfaab04826
Vusal <iamvusalalakbarov@gmail.com>
2025-04-24 22:45:57 +0400

    Created enums in types.ts

---
+++ src/lib/types.ts
@@ -0,0 +1,24 @@
+export enum Apparatus {
+  FloorExercise = "floor-exercise",
+  PommelHorse = "pommel-horse",
+  Rings = "rings",
+  Vault = "vault",
+  ParallelBars = "parallel-bars",
+  HorizontalBar = "horizontal-bar",
+}
+
+export enum Country {
+  AZE = "aze",
+  RUS = "rus",
+  TUR = "tur",
+  FRA = "fra",
+  ISR = "isr",
+  GER = "ger",
+}
+
+export enum Status {
+  Default,
+  Wait = "Wait",
+  Go = "Go",
+  InquirySubmitted = "Inquiry Submitted",
+}
